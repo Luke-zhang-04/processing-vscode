756d18c8c55a2854bcad345a550d98141d9beda3
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-05-21 18:07:31 -0400

    fix: don't show hover docs on single line comments

--- src/documentation.ts
+++ src/documentation.ts
@@ -13,7 +13,11 @@ import vscode from "vscode"
  * @param line - Contents of line
  * @param position - Position of hover
  */
-const getHoveredItem = (line: string, position: number): string => {
+const getHoveredItem = (line: string, position: number): string | undefined => {
+    if (/\/\//u.test(line.slice(0, position))) {
+        return
+    }
+
     const itemStart = (() => {
         let index = position
 
@@ -174,10 +178,14 @@ vscode.languages.registerHoverProvider(
     {
         provideHover: (document, position) => {
             const line = document.lineAt(position.line)
-            const item = getHoveredItem(
-                line.text,
-                position.character,
-            ) as keyof typeof documentation
+            const item = getHoveredItem(line.text, position.character) as
+                | keyof typeof documentation
+                | undefined
+
+            if (item === undefined) {
+                return
+            }
+
             const info = (documentation as Documentation)[item]
 
             if (!info) {
